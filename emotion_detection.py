ecd27653fd997a80e72c8076477e2a60c6735dd7
Hrutuja Mirgal <113509559+hrutujamirgal@users.noreply.github.com>
2024-10-17 16:28:36 +0530

    Create emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+
+    return response.json()  
+
+# Example usage:
+text = "I'm feeling great today!"
+emotion_result = emotion_detector(text)
+print(emotion_result)
